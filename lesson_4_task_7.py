983a6cbb1d723b1307083faf52be3f2cdfb73b29
swetlana <gubina.sv@big3.ru>
2019-03-14 23:15:28 +0300

    lesson 4 task 7

---
+++ lesson_4_task_7.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/login.php")
+    driver.find_element_by_name('username').send_keys('admin')
+    driver.find_element_by_name('password').send_keys('admin')
+    driver.find_element_by_name('login').click()
+
+    c = driver.find_elements_by_css_selector('[id="app-"]')
+
+    for i in range (len(c)):
+        elems = driver.find_elements_by_css_selector('[id="app-"]')
+        elems[i].click()
+        assert(driver.find_elements_by_css_selector("h1"))
+        elems = driver.find_elements_by_css_selector('[id="app-"]')
+        in_elems = elems[i].find_elements_by_css_selector("li")
+        if in_elems:
+            for y in range (len(in_elems)):
+                elems = driver.find_elements_by_css_selector('[id="app-"]')
+                in_elems1 = elems[i].find_elements_by_css_selector("li")
+                in_elems1[y].click()
+                assert (driver.find_elements_by_css_selector("h1"))
