b7e3d360556394554ea352b25234b09c2fc9767a
swetlana <gubina.sv@big3.ru>
2019-03-24 00:02:03 +0300

    lesson 6 task 12

--- lesson_6_task_11.py
+++ lesson_6_task_11.py
@@ -1,5 +1,20 @@
 import pytest
 from selenium import webdriver
+import datetime
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+def logout(driver):
+    driver.find_element_by_css_selector('[id="box-account"] [href*=logout]').click()
+
+
+def login(driver, login, password):
+    driver.find_element_by_css_selector('[name="email"]').send_keys(login)
+    driver.find_element_by_css_selector('[name="password"]').send_keys(password + Keys.ENTER)
+
 
 @pytest.fixture
 def driver(request):
@@ -10,8 +25,9 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
+
 def test_example(driver):
-    driver.get("http://localhost/litecart")
+    driver.get("http://litecart.stqa.ru")
 
     driver.find_element_by_css_selector('[id="box-account-login"] a').click()
 
@@ -19,10 +35,28 @@ def test_example(driver):
     driver.find_element_by_css_selector('[name="lastname"]').send_keys("O'Shey")
     driver.find_element_by_css_selector('[name="address1"]').send_keys("Moscow")
     driver.find_element_by_css_selector('[name="postcode"]').send_keys("12345")
-    driver.find_element_by_css_selector('[class="select2-selection__rendered"]').click()
+    driver.find_element_by_css_selector('[name="city"]').send_keys('Moscow again')
+
+    select = Select(driver.find_element_by_css_selector('[name="country_code"]'))
+    select.select_by_visible_text('United States')
+
     import time
-    time.sleep(5)
+    time.sleep(2)
+    wait = WebDriverWait(driver, 2)
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[id="create-account"] select[name="zone_code"]')))
 
+    select1 = Select(driver.find_element_by_css_selector('[id="create-account"] select[name="zone_code"]'))
+    select1.select_by_visible_text('Alaska')
 
+    email = 'test{}@test.com'.format(datetime.datetime.timestamp(datetime.datetime.now()))
+    driver.find_element_by_css_selector('[name="email"]').send_keys(email)
+    driver.find_element_by_css_selector('[name="phone"]').send_keys('9051234567')
+    password = '123456'
+    driver.find_element_by_css_selector('[name="password"]').send_keys(password)
+    driver.find_element_by_css_selector('[name="confirmed_password"]').send_keys(password)
 
+    driver.find_element_by_css_selector('[name="create_account"]').click()
 
+    logout(driver)
+    login(driver, email, password)
+    logout(driver)
